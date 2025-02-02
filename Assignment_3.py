5b6c34a881d4cd349ad0333c3568f13a593a41b3
Siddhant shrestha <sshrestha_be23@thapar.edu>
2025-02-02 21:06:21 +0530

    ASS_3

---
+++ Assignment_3.py
@@ -0,0 +1,89 @@
+#Q.1
+import pandas as pd
+
+data = {
+    "Tid": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Refund": ["Yes", "No", "No", "Yes", "No", "No", "Yes", "No", "No", "No"],
+    "Marital Status": ["Single", "Married", "Single", "Married", "Divorced", "Married", "Divorced", "Single", "Single", "Single"],
+    "Taxable Income": ["125K", "100K", "70K", "120K", "95K", "60K", "220K", "85K", "75K", "90K"],
+    "Cheat": ["No", "No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
+
+#Q.2
+import pandas as pd
+
+data = {
+    "Tid": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Refund": ["Yes", "No", "No", "Yes", "No", "No", "Yes", "No", "No", "No"],
+    "Marital Status": ["Single", "Married", "Single", "Married", "Divorced", "Married", "Divorced", "Single", "Single", "Single"],
+    "Taxable Income": ["125K", "100K", "70K", "120K", "95K", "60K", "220K", "85K", "75K", "90K"],
+    "Cheat": ["No", "No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
+
+selected_rows = df.loc[[0, 4, 7, 8]]
+
+print(selected_rows)
+
+
+
+#Q.3.1
+import pandas as pd
+
+data = {
+    "Tid": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Refund": ["Yes", "No", "No", "Yes", "No", "No", "Yes", "No", "No", "No"],
+    "Marital Status": ["Single", "Married", "Single", "Married", "Divorced", "Married", "Divorced", "Single", "Single", "Single"],
+    "Taxable Income": ["125K", "100K", "70K", "120K", "95K", "60K", "220K", "85K", "75K", "90K"],
+    "Cheat": ["No", "No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
+
+selected_rows = df.loc[3:7]
+
+print(selected_rows)
+
+#Q.3.2
+import pandas as pd
+
+data = {
+    "Tid": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Refund": ["Yes", "No", "No", "Yes", "No", "No", "Yes", "No", "No", "No"],
+    "Marital Status": ["Single", "Married", "Single", "Married", "Divorced", "Married", "Divorced", "Single", "Single", "Single"],
+    "Taxable Income": ["125K", "100K", "70K", "120K", "95K", "60K", "220K", "85K", "75K", "90K"],
+    "Cheat": ["No", "No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
+
+selected_data = df.iloc[4:9, 2:5]
+
+print(selected_data)
+ 
+#Q.3.3
+import pandas as pd
+
+data = {
+    "Tid": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Refund": ["Yes", "No", "No", "Yes", "No", "No", "Yes", "No", "No", "No"],
+    "Marital Status": ["Single", "Married", "Single", "Married", "Divorced", "Married", "Divorced", "Single", "Single", "Single"],
+    "Taxable Income": ["125K", "100K", "70K", "120K", "95K", "60K", "220K", "85K", "75K", "90K"],
+    "Cheat": ["No", "No", "No", "No", "Yes", "No", "Yes", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
+
+selected_columns = df.iloc[:, 1:4]
+
+print(selected_columns)
+
+
+#Q.4
